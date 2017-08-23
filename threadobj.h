0c200d5b797d8ae4f5d4be131775192ebc4ad392
keidoekd2345 <liuzhenhua@localhost.localdomain>
2017-08-23 04:58:29 -0400

    20180823

--- threadobj.h
+++ threadobj.h
@@ -1,15 +1,15 @@
-#ifndef _threadobj_h
-#define _threadobj_h
+#ifndef _THREADOBJ_H_
+#define _THREADOBJ_H_
 #include <pthread.h>
 class CThread_mutex
 {
     public:
     CThread_mutex();
-    CThread_mutex & operator= ( const CThread_mutex & m );
-    CThread_mutex(CThread_mutex & m);
+    CThread_mutex & operator=( const CThread_mutex & m );
+    CThread_mutex(CThread_mutex &m);
     bool init_mutex();
     bool lock_mutex();
-    bool unlock_mutex();
+    bool unlock_mutex();        
     ~CThread_mutex();
     private:
     pthread_mutex_t m_mutex;
